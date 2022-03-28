59edfde78233d32948ca8598e2b330a001ad0fca
RenBryant <bryantrlauren2300@gmail.com>
2022-03-28 01:56:35 -0400

    added to Javascript and readme

--- README.md
+++ README.md
@@ -8,3 +8,23 @@ This is a password generator that was created to practice Javascript. I was give
 work so that a random password will be given at the push of a button.
 
 FIrst I added the function to generate the password, made sure the button works, and that the characters will display on the screen.
+
+Then I added arrays for numbers, special characters, upper case letters, and lower case letters. Another array with every one of these was
+created after that.
+
+Then I added a validation of the length to be between 8 and 128, and if not then the page would spit out an alert and an undefined password.
+
+I gave the user the option to choose between what characters they want to use for their password with prompts and concats. If the user doesn't
+want any of the four options, it spits out an alert and then gives the user an undefined password.
+
+Then I made a for Loop that will spit out the options for the password based on what they chose.
+
+However it doesn't seem to work.
+
+I tried to make the password random as well using Math.floor(Math.random)
+
+However this also doesn't seem to work.
+
+I feel as if both of these problems co-relate, but no matter what little changes I do, no matter how long I look at it, I cannot seem to find the issue. Please help.
+
+Thanks for checking in on my repository!
--- assets/Java.js
+++ assets/Java.js
@@ -1,16 +1,72 @@
-// Assignment code here
-
+//array options 
+var specialChar = ['@', '#', '$', '1', '%', '&', '*', '-', '_', '+', '=', '?', '!'];
+var numChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var lowerCaseChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCaseChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var passwordOptions = ['@', '#', '$', '1', '%', '&', '*', '-', '_', '+', '=', '?', '!', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd',
+ 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+ 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var finalPassword = [];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-    console.log ("Clicked")
-    //this is to make sure my button works
-    
-    
+  var length = parseInt(prompt('How many characters would you like your password to contain?'))
 
-    return "Password goes here";
+  
+
+  //validates length
+  if (length < 8) {
+    alert('Password length must be at least 8 characters')
+    return
+  }
+
+  if (length > 128) {
+    alert('Password length must be at less than 128 characters')
+  }
+
+  //character confirms
+  var hasSpecialChar = confirm('Click OK to include special characters')
+
+  var hasNumChar = confirm('Click OK to include numbers')
+
+  var hasLwrCaseChar = confirm('Click OK to include lowercase characters')
+
+  var hasUpperCaseChar = confirm('Click OK to include uppercase characters')
+
+  //character validation
+  if (hasSpecialChar === false && hasNumChar === false && hasLwrCaseChar === false && hasUpperCaseChar === false) {
+    alert('Must select at least one character type!')
+    return
+  }
+
+
+  //if user confirms character options, add options to passwordOptions array
+  if (hasSpecialChar) {
+    passwordOptions = passwordOptions.concat(specialChar);
+  }
+
+  if (hasNumChar) {
+    passwordOptions = passwordOptions.concat(numChar);
+  }
+
+  if (hasUpperCaseChar){
+    passwordOptions = passwordOptions.concat(upperCaseChar);
+  }
+
+
+  for (var i = 0; i < length; i++ ){
+    //logic
+    var randomPassword = Math.floor(Math.random() * length);
+    finalPassword = finalPassword + passwordOptions[randomPassword];
+    return passwordOptions.join('');
+  }
+  
+  console.log("this is the array of which the logic chooses the characters", passwordOptions)
+
+  console.log('once loop finishes, characters are inserted here', finalPassword)
+  return finalPassword;
 
 }
 
